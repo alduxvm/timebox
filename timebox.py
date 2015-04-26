1d6fd8fd803132d38717eeb3ab7a4e91882309b2
Aldo Vargas <alduxvm@gmail.com>
2015-04-26 15:50:22 +0100

    Add inclination
    Add the inclination to print according to angle.

--- timebox.py
+++ timebox.py
@@ -21,7 +21,7 @@ debug = True
 
 # MultiWii Initialization
 #board = MultiWii("/dev/ttyUSB0")
-#board = MultiWii("/dev/tty.usbserial-A801WZA1")
+board = MultiWii("/dev/tty.usbserial-A801WYAE")
 
 # CSV data Initialization
 # Read CSV file
@@ -42,7 +42,10 @@ for i in range(len(events)):
 	events[i][0] = tempdate.strftime('%d %b %Y')
 
 # Printer 
-printer = ThermalPrinter(serialport='/dev/ttyAMA0')
+try:
+	printer = ThermalPrinter(serialport='/dev/ttyAMA0')
+except:
+	pass
 
 sizeX = 320
 sizeY = 240
@@ -65,14 +68,14 @@ def boxAngle():
 	canvas.delete(a)
 	canvas.delete(c)
 	canvas.delete(b)
-	#board.getData(MultiWii.ATTITUDE)
+	board.getData(MultiWii.ATTITUDE)
 	running = 1
-	attitude = {'angx':0,'angy':0,'heading':0,'elapsed':0,'timestamp':0}
-	arcStart = attitude['angx']+15+90
-	arcEnd = attitude['angx']-15+90
-	r = int(utils.limit(utils.mapping(attitude['angx'], -90, 10, 255, 0),0,255))
-	g = int(utils.limit(utils.mapping(attitude['angx'], -20, 20,  200, 255),0,255))
-	b = int(utils.limit(utils.mapping(attitude['angx'], -10, 90, 0, 255),0,255)) 
+	#attitude = {'angx':0,'angy':0,'heading':0,'elapsed':0,'timestamp':0}
+	arcStart = board.attitude['angx']+15+90
+	arcEnd = board.attitude['angx']-15+90
+	r = int(utils.limit(utils.mapping(board.attitude['angx'], -90, 10, 255, 0),0,255))
+	g = int(utils.limit(utils.mapping(board.attitude['angx'], -20, 20,  200, 255),0,255))
+	b = int(utils.limit(utils.mapping(board.attitude['angx'], -10, 90, 0, 255),0,255)) 
 	#print "angle= %0.2f r= %d g= %d b= %d" % (board.attitude['angx'],r,g,b)
 	#canvas.create_circle_arc(100, 120, 50, fill="black", style="arc", outline=utils.rgb_to_hex((r, g, b)), width=10, start=0, end=180)
 	c = canvas.create_circle(sizeX/2, sizeY/2, circleradius, fill="black", outline=utils.rgb_to_hex((r, g, b)), width=10)
@@ -98,37 +101,48 @@ def click(event):
 	global b, update, running, printer, events, board
 	canvas.delete(b)
 	b = canvas.create_circle(sizeX/2, sizeY/2, 30, fill="red", outline="white", width=1)
-	tk.update()
+	#tk.update()
 	#tk.after_cancel(update)
-	print "imprimiendo..."
 	#time.sleep(0.5)
-	valor = random.randint(0,len(events)-1)
-	#valor = utils.mapping(board.attitude)
-	message = "\n%s\nOn %s\n%s\n\nMore info: %s\n\n" % (events[valor][1],events[valor][0],events[valor][2],events[valor][3])
+	#valor = random.randint(0,len(events)-1)
+	if board.attitude['angx'] == 0.0:
+		valor = random.randint(0,len(events)-1)
+	else:
+		valor = int(utils.limit(utils.mapping(board.attitude['angx'],-110,110,0,len(events)-1),0,len(events)-1))
+	print "Start printing... ang %0.2f, value %d\n" % (board.attitude['angx'],valor)
+	try: 
+		url = events[valor][3]
+	except:
+		url = "No URL... sorry"
+
+	message = "%s\nOn %s\n%s\nMore info: %s\n" % (events[valor][1],events[valor][0],events[valor][2],url)
 	print message
-	printer.justify("C")
-	printer.inverse_on()
-	printer.bold_on()
-	printer.print_text("TIME BOX")
-	printer.inverse_off()
-	printer.print_text("\n______________\n")
-	printer.bold_off()
-	printer.print_text("According to:\n")
-	printer.bold_on()
-	printer.print_text(str(valor))
-	printer.print_text(" degrees")
-	printer.bold_off()
-	printer.print_text("\nof inclination\n______________\n\n")
-	printer.bold_on()
-	printer.print_text(events[valor][1])
-	printer.bold_off()
-	printer.print_text("\n")
-	printer.print_text(events[valor][0])
-	printer.print_text("\n\n")
-	printer.print_text(events[valor][2])
-	printer.print_text("\n\nMore info:\n")
-	printer.print_text(events[valor][3])
-	printer.print_text("\n\n:)\n____________________________\n\n\n")
+	try:
+		printer.justify("C")
+		printer.inverse_on()
+		printer.bold_on()
+		printer.print_text("TIME BOX")
+		printer.inverse_off()
+		printer.print_text("\n______________\n")
+		printer.bold_off()
+		printer.print_text("According to:\n")
+		printer.bold_on()
+		printer.print_text(str(board.attitude['angx']))
+		printer.print_text(" degrees")
+		printer.bold_off()
+		printer.print_text("\nof inclination\n______________\n\n")
+		printer.bold_on()
+		printer.print_text(events[valor][1])
+		printer.bold_off()
+		printer.print_text("\n")
+		printer.print_text(events[valor][0])
+		printer.print_text("\n\n")
+		printer.print_text(events[valor][2])
+		printer.print_text("\n\nMore info:\n")
+		printer.print_text(events[valor][3])
+		printer.print_text("\n\n:)\n____________________________\n\n\n\n")
+	except:
+		print "Error while printing, perhaps no printer attached."
 	#update = boxAngle()
 	#t = canvas.create_text(sizeX/2, sizeY/2,text="Printing...", fill="white", font=("Helvetica", 20))
     #tk.quit()
